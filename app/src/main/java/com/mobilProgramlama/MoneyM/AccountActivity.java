9fb6258adb7feddf17430e949226c945a3ed88c5
Mustafa Alpay Ateş <mustafa.alpay.ates@hotmail.com>
2021-05-31 07:29:39 +0300

    Initial commit

---
+++ app/src/main/java/com/mobilProgramlama/MoneyM/AccountActivity.java
@@ -0,0 +1,60 @@
+package com.mobilProgramlama.MoneyM;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class AccountActivity extends AppCompatActivity {
+
+    private Toolbar settingsToolbar;
+    private TextView userEmail;
+    private Button logoutBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        settingsToolbar = findViewById(R.id.my_Feed_Toolbar);
+        setSupportActionBar(settingsToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Hesabım");
+
+        logoutBtn = findViewById(R.id.logoutBtn);
+        userEmail  = findViewById(R.id.userEmail);
+
+        userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(AccountActivity.this)
+                        .setTitle("MoneyM")
+                        .setMessage("Çıkış yapmak istediğine emin misin?")
+                        .setCancelable(false)
+                        .setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                FirebaseAuth.getInstance().signOut();
+                                Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
+                                startActivity( intent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Hayır", null)
+                        .show();
+            }
+        });
+    }
+}
